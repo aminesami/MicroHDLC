c9b31822ff42c9699083b7b06d4fb64058123e06
Amine Sami <amine.sami.1997@gmail.com>
2020-11-28 16:13:01 -0500

    bit unstuffing

--- common/Decoder.java
+++ common/Decoder.java
@@ -21,4 +21,49 @@ public class Decoder {
             res[i] = bytes.get(i);
         return res;
     }
+
+    public static byte[] bitUnstuff (byte[] data) throws Exception {
+        int successiveOnes = 0;
+        int nBits = 0;
+        int currentByte = 0;
+        
+        ArrayList<Byte> bytes = new ArrayList<Byte>();
+        
+        for (int j = 0; j < data.length; j++) {
+            byte b = data[j];
+            for (int i = 0; i < 8; i++) {
+                int bit = (b >> (7 - i)) & 1;
+                if (bit == 1)
+                    successiveOnes++;
+                else
+                    successiveOnes = 0;
+                if (successiveOnes == 5) {
+                    if (i == 7) { // end of byte special case
+                        if (j + 1 == data.length)
+                            throw new Exception("corrupted data");
+
+                        b = data[++j];
+                        i = -1;
+                    }
+                    int nextBit = (b >> (7 - (++i))) & 1;
+                    if (nextBit != 0)
+                        throw new Exception("corrupted data");
+                    // reaching here means we skipped a zero following five 1's
+                    successiveOnes = 0;
+                }
+                currentByte = (currentByte << 1) | bit;
+                nBits++;
+                if (nBits == 8) {
+                    bytes.add((byte) currentByte);
+                    nBits = currentByte = 0;
+                }
+            }
+        }
+
+        byte[] res = new byte[bytes.size()];
+        for (int i = 0; i < bytes.size(); i++)
+            res[i] = bytes.get(i);
+
+        return res;
+    }
 }
